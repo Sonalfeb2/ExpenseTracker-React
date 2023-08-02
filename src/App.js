1f5bc4ab660652e863f59fd19a60459b96948547
Sonal Agrawal <sonalfeb7@gmail.com>
2023-08-02 17:53:53 +0530

    Create Expense Tracker Component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseTracker from "./components/ExpenseTracker";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseTracker.js
@@ -0,0 +1,12 @@
+const ExpenseTracker = () => {
+  return (
+    <div>
+      <h1>Expense Tracker</h1>
+      <p>Food Rs 120</p>
+      <p>Petrol Rs 100</p>
+      <p>Movie Rs 200</p>
+    </div>
+  );
+};
+
+export default ExpenseTracker;
