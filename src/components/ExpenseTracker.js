2df1d9dbad1e5a94834b9ac5eddf754f13b17b31
Sonal Agrawal <sonalfeb7@gmail.com>
2023-08-03 19:30:21 +0530

    Add ExpenseDetail Component & Add Styling in Date Component

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,20 +1,11 @@
 import "./ExpenseTracker.css";
 import ExpenseDate from "./ExpenseDate";
+import ExpenseDetail from "./ExpenseDetail";
 const ExpenseTracker = props => {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>
-          {props.location}
-        </h2>
-        <h2>
-          {props.title}
-        </h2>
-        <div className="expense-item__price">
-          Rs.{props.amount}
-        </div>
-      </div>
+      <ExpenseDetail amount={props.amount} location={props.location} title={props.title}/>
     </div>
   );
 };
