9a71a706c88a585d6b92ada52e804d27aaf9c8c0
Sonal Agrawal <sonalfeb7@gmail.com>
2023-08-29 18:26:43 +0530

    Added inital Expensed Items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,13 +5,39 @@ import ExpenseForm from "./ExpenseForm";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 const Expenses = () => {
-  const [expenseArr, setExpenseArr] = useState([]);
   const[filterYear, setFilterYear] = useState('2020');
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      location: "Commercial Street",
+      date : new Date("2021-08-09")
+    },
+    { id: "e2", title: "New TV", amount: 799.49,location:"Electronic City",date : new Date("2021-08-09") },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      location: "Tansi Honda",
+      date : new Date("2021-08-09")
+
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      location: "Home@99",
+      date : new Date("2021-08-09")
+    }
+  ];
+  
+  const [expenseArr, setExpenseArr] = useState(expenses);
   const filterHandler = selectedYear=>{
     setFilterYear(selectedYear);
   }
   const expenseHandler = expenseObj => {
-    setExpenseArr(expenseArr => [...expenseArr, expenseObj]);
+    setExpenseArr(expenseArr => [expenseObj, ...expenseArr]);
     console.log(expenseArr)
   };
 
