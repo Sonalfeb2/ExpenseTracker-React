6586eced5796260e6ec5339dcf63c79bf5eb46f4
Sonal Agrawal <sonalfeb7@gmail.com>
2023-08-30 19:43:08 +0530

    Challenges on Conditional Rendering

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import './ExpenseForm.css'
+import "./ExpenseForm.css";
 const ExpenseForm = props => {
+  const [showForm, setShowForm] = useState(false);
   const [expenseObj, setExpenseObj] = useState({
     title: "",
     amount: 0,
     location: "",
-    date:''
+    date: ""
   });
   const submitHandler = () => {
     const expenseData = {
@@ -17,68 +18,93 @@ const ExpenseForm = props => {
       title: "",
       amount: 0,
       location: "",
-      date:'',
+      date: ""
     });
-
-    console.log(expenseData);
+    setShowForm(!showForm);
   };
   return (
-    <div className="new-expense">
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={expenseObj.title}
-            onChange={e => {
-              setExpenseObj(prevState => ({
-                ...prevState,
-                title: e.target.value
-              }));
-            }}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Location</label>
-          <input
-            type="text"
-            id="location"
-            value={expenseObj.location}
-            onChange={e => {
-              setExpenseObj(prevState => ({
-                ...prevState,
-                location: e.target.value
-              }));
-            }}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={expenseObj.amount}
-            onChange={e => {
-              setExpenseObj(prevState => ({
-                ...prevState,
-                amount: e.target.value
-              }));
-            }}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31"  value={expenseObj.date} onChange={e => {
-              setExpenseObj(prevState => ({
-                ...prevState,
-                date: e.target.value
-              }));
-            }} />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button onClick={submitHandler}>Submit</button>
+    <div>
+      {showForm &&
+        <div className="new-expense">
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={expenseObj.title}
+                onChange={e => {
+                  setExpenseObj(prevState => ({
+                    ...prevState,
+                    title: e.target.value
+                  }));
+                }}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Location</label>
+              <input
+                type="text"
+                id="location"
+                value={expenseObj.location}
+                onChange={e => {
+                  setExpenseObj(prevState => ({
+                    ...prevState,
+                    location: e.target.value
+                  }));
+                }}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={expenseObj.amount}
+                onChange={e => {
+                  setExpenseObj(prevState => ({
+                    ...prevState,
+                    amount: e.target.value
+                  }));
+                }}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={expenseObj.date}
+                onChange={e => {
+                  setExpenseObj(prevState => ({
+                    ...prevState,
+                    date: e.target.value
+                  }));
+                }}
+              />
+            </div>
+          </div>
+          <div>
+            <button className="new-expense__actions" onClick={submitHandler}>
+              Submit
+            </button>
+            <button
+              className="new-expense__actions"
+              onClick={() => setShowForm(!showForm)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>}
+      <div>
+        {!showForm &&
+          <button
+            className="new-expense__actions"
+            onClick={() => setShowForm(!showForm)}
+          >
+            Add New Expense
+          </button>}
       </div>
     </div>
   );
